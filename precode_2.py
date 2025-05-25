f4c4a4b4926aa5794113f7f91cca2b0fbe7ecab6
Dmitriy Reshetnik <dm.reshetnik@yandex.ru>
2025-05-25 23:17:30 +0300

    Add test cases to precode_3.py

--- precode_2.py
+++ precode_2.py
@@ -3,10 +3,7 @@ from miles import HobbitsPriorityStatus
 
 class TestHobbitsPriorityStatus:
 
-    # допиши тест, который добавит в программу нового хоббита с именем Голлум,
-    # начислит ему 50 миль и проверит его статус в системе
     def test_add_miles_and_check_status(self):
-        # использовали объект класса HobbitsPriorityStatus, чтобы вызвать его методы
         hobbit = 'Голлум'
         hobbits_priority = HobbitsPriorityStatus()
         hobbits_priority.add_new_hobbit(hobbit)
--- precode_3.py
+++ precode_3.py
@@ -2,10 +2,20 @@ import pytest
 
 from miles import HobbitsPriorityStatus
 
+
 class TestHobbitsPriorityStatus:
 
-    # добавь параметризацию
-    ...
-    def test_add_miles_and_check_status(self, ...): # допиши параметры функции
-        # создай нового хоббита в системе, добавь ему мили и проверь его статус
+    @pytest.mark.parametrize(
+        'miles, priority_status',
+        [
+            [299, 'Classic'],
+            [300, 'Silver'],
+        ]
+    )
+    def test_add_miles_and_check_status(self, miles, priority_status):
         hobbits_priority = HobbitsPriorityStatus()
+        hobbit = 'Голлум'
+        hobbits_priority.add_new_hobbit(hobbit)
+        hobbits_priority.add_miles_to_hobbit(hobbit, miles)
+        assert hobbits_priority.hobbits_miles[hobbit] == miles
+        assert hobbits_priority.get_status(hobbit) == priority_status
