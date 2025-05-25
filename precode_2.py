f6700b1f9083efa3fc1f7bf26d4d3d76fa0ac8b2
Dmitriy Reshetnik <dm.reshetnik@yandex.ru>
2025-05-25 22:00:39 +0300

    Add 'test_add_miles_and_check_status' in precode_2.py

--- precode_2.py
+++ precode_2.py
@@ -1,4 +1,3 @@
-# импортируем класс HobbitsPriorityStatus из файла miles.py
 from miles import HobbitsPriorityStatus
 
 
@@ -8,5 +7,9 @@ class TestHobbitsPriorityStatus:
     # начислит ему 50 миль и проверит его статус в системе
     def test_add_miles_and_check_status(self):
         # использовали объект класса HobbitsPriorityStatus, чтобы вызвать его методы
+        hobbit = 'Голлум'
         hobbits_priority = HobbitsPriorityStatus()
-        ...
+        hobbits_priority.add_new_hobbit(hobbit)
+        hobbits_priority.add_miles_to_hobbit(hobbit, 50)
+        assert hobbits_priority.hobbits_miles[hobbit] == 50
+        assert hobbits_priority.get_status(hobbit) == 'Classic'
